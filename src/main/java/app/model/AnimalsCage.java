fee6ba6f6d6b024c79efd2d401a322ea9acda692
alena.goryunova <alena.goryunova@rtlabs.ru>
2024-01-06 13:37:47 +0300

    Создан новый класс подходящий под тип бина animal, реализовано автоматическое связывание бинов

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,29 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
-    private Animal animal;
+    @Qualifier("dog") // для связывания по имени
+    private Animal animal; // для связывания по имени еще можно заменить имя переменной на cat или dog
+
+    @Autowired
+    private Timer timer; // автоматическое связывание с бином таймера
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime()); // замена вместо new Timer().getTime()
         System.out.println("________________________");
+    }
+    public Timer getTimer() { // для теста
+        return timer;
     }
 }
---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+//@Primary можно использовать для подстановки бина dog в случае если есть несколько бинов одного типа animal
+public class Dog extends Animal{
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
